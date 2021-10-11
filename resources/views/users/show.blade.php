a8bec645f2504382dd8cc3d20930efbb12d3e050
Driccioli <driccioli53@gmail.com>
2021-10-11 18:29:22 +0200

    Created potential three-column layout

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="p-5">
+    <div class="p-1 container">
         {{-- Attributes: 
             name 
             lastname
@@ -15,32 +15,48 @@
             timestamps(optional)
             rating (optional)
         --}}
+        <div class="row flex-nowrap">
+            <div class="col-4 show-column contacts">
+                <img class="profile_picture" src="{{$user->profile_pic}}" alt="{{$user->name}} {{$user->name}}'s photo">
+                <h4>{{$user->qualification}} {{$user->name}} {{$user->lastname}}</h4>
+                {{-- Le 5 stelle indicano la media dei voti ricevuti. Magari conviene farlo in Vue? --}}
+                <div class="rating">
+                    {{-- Attualmente è solo un placeholder, le cinque stelle devono riempirsi a seconda della media dei voti --}}
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                </div>
+                {{-- Il testo di minor_text dovrebbe essere piccolo e in grigio --}}
+                <span class="minor_text">Last updated: {{$user->updated_at}}</span>
+                <h4>Contatti</h4>
+                <div>
+                    <span>Email: {{$user->email}} <br></span>
+                    <span>Indirizzo: {{$user->address}} <br></span>
+                </div>
+            </div>
+
+            <div class="col-4 show-column professional-info">
+                {{-- Contiene informazioni professionali come curriculum, specializzazioni e tariffe(?) --}}
+                <h2>Curriculum</h2>
+                <p class="text-break">
+                    {{$user->cv}}
+                Did you know that Bootstrap is like a printer? Refuses to work and has a new problem every time!
+                superduperlongwordireallyhopebootstrapbreaksthisoneanddoesntoverflowtotheothercontainercompletelywreckingmybeautifullayout
+                </p>
+                
+            </div>
+
+            <div class="col-4 show-column reviews">
+                {{-- Contiene le recensioni e lo spazio per crearne una --}}
+                <h2>Recensioni</h2>
+                {{-- Inserire componente per le review --}}
+            </div>
+        </div>
         
-        <img class="profile_picture" src="{{$user->profile_pic}}" alt="{{$user->name}} {{$user->name}}'s photo">
-        <h4>{{$user->qualification}} {{$user->name}} {{$user->lastname}}</h4>
-        {{-- Le 5 stelle indicano la media dei voti ricevuti. Magari conviene farlo in Vue? --}}
-        <div class="rating">
-            {{-- Attualmente è solo un placeholder, le cinque stelle devono riempirsi a seconda della media dei voti --}}
-            <i class="bi bi-star-fill"></i>
-            <i class="bi bi-star-fill"></i>
-            <i class="bi bi-star-fill"></i>
-            <i class="bi bi-star-fill"></i>
-            <i class="bi bi-star-fill"></i>
-        </div>
-        {{-- Il testo di minor_text dovrebbe essere piccolo e in grigio --}}
-        <span class="minor_text">Last updated: {{$user->updated_at}}</span>
-        <h4>Contatti</h4>
-        <div>
-            <span>Email: {{$user->email}} <br></span>
-            <span>Indirizzo: {{$user->address}} <br></span>
-            <span>CV: {{$user->cv}} <br></span>
-        </div>
         
-        <div class="reviews">
-            
-            <h2>Recensioni</h2>
-            {{-- Inserire componente per le review --}}
-        </div>
+        
          
     </div>
 @endsection
