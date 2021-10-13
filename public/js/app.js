81201c00ce99f4b77e17e88da8daaee3d6544cbd
vvalcarcell <valcarcellvalentina@gmail.com>
2021-10-13 19:55:27 +0200

    ok

--- public/js/app.js
+++ public/js/app.js
@@ -52140,8 +52140,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/simonedimeglio/Desktop/BDoctors-1/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/simonedimeglio/Desktop/BDoctors-1/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Valentina\Desktop\BDoctors\BDoctors\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Valentina\Desktop\BDoctors\BDoctors\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
