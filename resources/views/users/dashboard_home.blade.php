a5b32f22d38e75b2aed4489c19ea7be07cb5df74
CarloD39 <carlodurand@hotmail.it>
2021-10-19 09:21:10 +0200

    dashboard home

--- resources/views/users/dashboard_home.blade.php
+++ resources/views/users/dashboard_home.blade.php
@@ -1,89 +1,112 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
-    <div class="row justify-content-center">
-        
-        <div class="col-md-2">
-            <div class="card cards-profile">
-                <div class="card-body cards-menu">
-                    <div class="dashboard-links">
-                        <h6>
-                            <a href="{{route('users.edit', $loggedUser)}}">Modifica Profilo <div><i class="bi bi-pencil-square"></i></div></a>
-                        </h6>
-                    </div>    
+    <div class="row">
+        <div class="col-md-12">
+            <div class="dash-background">
+                <div class="dash-title">
+                    <h1>Benvenuto nella tua Dashboard</h1> 
+                </div>
+            </div> 
+        </div>
+    </div>
+</div>
+
+
+<div class="main-dash">
+
+    
+
+    <div class="container">
+        <div class="row justify-content-center">
+
+            <div class=" col-lg-2 col-md-6 col-sm-6">
+                <div class="card cards-profile">
+                    <div class="card-body  cards-menu">
+                        <div class="dashboard-links">                                                       
+                            <a href="{{route('home')}}"><i class="bi bi-list"></i></a>
+                            <div class="on-hover-icon">Menu</div>
+                        </div> 
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-2">
-            <div class="card cards-profile">   
-                <div class="card-body cards-menu">
-                    <div class="dashboard-links">
-                        <h6>
-                            <a href="{{route('messages')}}">Messaggi <div><i class="bi bi-chat-left-dots"></i></div></a>
-                        </h6>
-                    </div>    
-                </div>
-            </div>  
-        </div>   
 
-        <div class="col-md-2">
-            <div class="card cards-profile">
-                <div class="card-body cards-menu">
-                    <div class="dashboard-links">
-                        <h6>
-                            <a href="{{route('reviews')}}">Recensioni <div><i class="bi bi-card-text"></i></div></a>
-                        </h6>
-                    </div>    
+            <div class=" col-lg-2 col-md-6 col-sm-6 cards-profile-col">
+                <div class="card cards-profile">
+                    <div class="card-body cards-menu">
+                        <div class="dashboard-links">                            
+                            <a href="{{route('users.edit', $loggedUser)}}"><i class="bi bi-pencil-square"></i></a>
+                            <div class="on-hover-icon">Edit</div>
+                        </div>    
+                    </div>
                 </div>
             </div>
-        </div>
+            <div class=" col-lg-2 col-md-6 col-sm-6 cards-profile-col">
+                <div class="card cards-profile">   
+                    <div class="card-body cards-menu">
+                        <div class="dashboard-links">                            
+                            <a href="{{route('messages')}}"><i class="bi bi-chat-left-dots"></i></a>
+                            <div class="on-hover-icon">Messaggi</div>
+                        </div>    
+                    </div>
+                </div>  
+            </div>   
 
-        <div class="col-md-2">
-            <div class="card cards-profile">
-                <div class="card-body cards-menu">
-                    <div class="dashboard-links">
-                        <h6>
-                            <a href="">Sponsorizzazione <div><i class="bi bi-cart-plus"></i></div></a>
-                        </h6>
-                    </div>    
+            <div class=" col-lg-2 col-md-6 col-sm-6 cards-profile-col">
+                <div class="card cards-profile">
+                    <div class="card-body cards-menu">
+                        <div class="dashboard-links"> 
+                            <a href="{{route('reviews')}}"><i class="bi bi-card-text"></i></a>
+                            <div class="on-hover-icon">Recensioni</div>
+                        </div>    
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div class="col-md-2">
-            <div class="card cards-profile">
-                <div class="card-body cards-menu">
-                    <div class="dashboard-links">
-                        <h6>
-                            <a href="">Statistiche <div><i class="bi bi-bar-chart-line"></i></div></a>
-                        </h6>
-                    </div>    
+            <div class=" col-lg-2 col-md-6 col-sm-6 cards-profile-col">
+                <div class="card cards-profile">
+                    <div class="card-body cards-menu">
+                        <div class="dashboard-links">                           
+                            <a href=""><i class="bi bi-cart-plus"></i></a>
+                            <div class="on-hover-icon">Shop</div>
+                        </div>    
+                    </div>
                 </div>
             </div>
-        </div>
-                
 
-
-        <div class="col-md-10">
-            <div class="card">
-                <div class="card-header"><a href="{{route('home')}}">Dashboard di {{$loggedUser->name}} {{$loggedUser->lastname}}</a></div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                            
-                        </div>
-                    @endif
-                    @yield('dashboard')
+            <div class="col-lg-2 col-md-6 col-sm-6 cards-profile-col">
+                <div class="card cards-profile">
+                    <div class="card-body cards-menu">
+                        <div class="dashboard-links">
+                            <a href=""><i class="bi bi-bar-chart-line"></i></a>
+                            <div class="on-hover-icon">Statistiche</div>
+                        </div>    
+                    </div>
                 </div>
             </div>
-        </div>
-               
+                    
+
+
+            <div class="col-md-12">
             
-        
-        
+                    <div class="card-body-dash">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                                
+                            </div>
+                        @endif
+                        @yield('dashboard')
+                    </div>
+                
+            </div>
+                
+                
+            
+            
+        </div>
     </div>
 </div>
 @endsection
