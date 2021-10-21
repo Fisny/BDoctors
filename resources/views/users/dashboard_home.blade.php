ea9e5f89754f9b7fbb0617b6fd672e57b113da0a
simonedimeglio <simonedimeglio@icloud.com>
2021-10-21 16:32:32 +0200

    new buttons on dushboard

--- resources/views/users/dashboard_home.blade.php
+++ resources/views/users/dashboard_home.blade.php
@@ -20,6 +20,18 @@
 
                 <div class="container">
                     <div class="row justify-content-center dash-button-container">
+
+                        <div class=" col-lg-2 col-md-6 col-sm-6 cards-profile-col">
+                            <div class="custom-card custom-cards-profile" id="orangex">   
+                                <div class="card-body cards-menu">
+                                    <div class="dashboard-links">                            
+                                        <a href="{{ url('/') }}"><i class="bi bi-house"></i></a>
+                                        <div class="on-hover-icon">Home</div>
+                                    </div>    
+                                </div>
+                            </div>  
+                        </div>   
+
                         <div class=" col-lg-2 col-md-6 col-sm-6 cards-profile-col">
                             <div class="custom-card custom-cards-profile" id="bluex">
                                 <div class="card-body cards-menu" >
@@ -30,6 +42,7 @@
                                 </div>
                             </div>
                         </div>
+
                         <div class=" col-lg-2 col-md-6 col-sm-6 cards-profile-col">
                             <div class="custom-card custom-cards-profile" id="redx">   
                                 <div class="card-body cards-menu">
@@ -63,6 +76,17 @@
                             </div>
                         </div>
             
+                        <div class=" col-lg-2 col-md-6 col-sm-6 cards-profile-col">
+                            <div class="custom-card custom-cards-profile" id="violetx">   
+                                <div class="card-body cards-menu">
+                                    <div class="dashboard-links">                            
+                                        <a href="{{route('storic')}}"><i class="bi bi-currency-dollar"></i></a>
+                                        <div class="on-hover-icon">Storico</div>
+                                    </div>    
+                                </div>
+                            </div>  
+                        </div>   
+
                         <div class="col-md-12">
                         
                                 <div class="card-body-dash">
