56eda9684c5a5980806f33336160d63a63de22c2
simonedimeglio <simonedimeglio@icloud.com>
2021-10-21 17:02:44 +0200

    shop update

--- resources/views/app/sponsorship.blade.php
+++ resources/views/app/sponsorship.blade.php
@@ -3,12 +3,12 @@
 @section('content')
     <div class="container">
 
-        <div class="container portfolio animate__animated animate__bounceInDown">
+        <div class="container portfolio animate__animated animate__tada">
             <div class="row">
                 <div class="col-md-12">
                     <div class="heading">
                         <img src="https://image.ibb.co/cbCMvA/logo.png" />
-                        <div class="heading-title">Sponsorizzazioni disponibili all'acquisto</div>
+                        <div class="heading-title">Bdoctors Shop</div>
                     </div>
                 </div>
             </div>
@@ -19,24 +19,24 @@
                 </div>
                 <div class="sponsorship-list-container">
                     <div class="sponsorship-card">
-                        <h1>BRONZE</h1>
-                        <i class="bi bi-emoji-smile"></i>
+                        <h1>Bronze</h1>
+                        <i class="fas fa-hippo"></i>
                         <p>Piano base. Otterrai <b>24 ore</b> di sponsorizzazione al prezzo di 2,99€</p>
                         <a href="{{ url('/checkout/1') }}" class="sponsorship-button">
                             <h5>Acquista <i class="bi bi-piggy-bank"></i></h5>
                         </a>
                     </div>
                     <div class="sponsorship-card">
-                        <h1>SILVER</h1>
-                        <i class="bi bi-emoji-sunglasses"></i>
+                        <h1>Silver</h1>
+                        <i class="fas fa-crow"></i>
                         <p>Piano intermedio. Otterrai <b>72 ore</b> di sponsorizzazione al prezzo di 5,99€</p>
                         <a href="{{ url('/checkout/2') }}" class="sponsorship-button">
                             <h5>Acquista <i class="bi bi-piggy-bank"></i></h5>
                         </a>
                     </div>
                     <div class="sponsorship-card">
-                        <h1>GOLD</h1>
-                        <i class="bi bi-emoji-heart-eyes"></i>
+                        <h1>Gold</h1>
+                        <i class="fas fa-dragon"></i>
                         <p>Piano Super! Otterrai <b>144 ore</b> di sponsorizzazione al prezzo di 9,99€</p>
                         <a href="{{ url('/checkout/3') }}" class="sponsorship-button">
                             <h5>Acquista <i class="bi bi-piggy-bank"></i></h5>
